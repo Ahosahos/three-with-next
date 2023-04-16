ada18ba6089e48debcad4c3e246d125869501693
sofiah <sofiahsabihah@webbytes.com.my>
2023-04-16 21:57:22 +0800

    create a basic swinging cube

---
+++ src/models/swingingCube.js
@@ -0,0 +1,20 @@
+import React, { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+
+export default function SwingingCube(props) {
+  const mesh = useRef();
+
+  useFrame(({ clock }) => {
+    mesh.current.rotation.x = Math.sin(clock.getElapsedTime());
+  });
+
+  return (
+    <mesh
+      // {...props}
+      ref={mesh}
+    >
+      <boxGeometry args={[3, 3, 3]} />
+      <meshBasicMaterial color={"yellow"} />
+    </mesh>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,27 @@
+import SwingingCube from "@/models/swingingCube";
 import { Canvas } from "@react-three/fiber";
 
 export default function Home() {
   return (
-    <>
+    <div style={{ margin: "auto", height: "100vh", width: "100vw" }}>
       <Canvas>
-        {/* mesh + box + material + lights */}
-        <mesh>
-          <ambientLight intensity={1} />
-          <directionalLight color="red" position={[0, 0, 5]} />
-          <boxGeometry args={[2, 2, 2]} />
-          <meshStandardMaterial />
-        </mesh>
+        <ambientLight intensity={1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <SwingingCube />
       </Canvas>
-    </>
+    </div>
   );
+  // return (
+  //   <>
+  //     <Canvas>
+  //       {/* mesh + box + material + lights */}
+  //       <mesh>
+  //         <ambientLight intensity={1} />
+  //         <directionalLight color="red" position={[0, 0, 5]} />
+  //         <boxGeometry args={[2, 2, 2]} />
+  //         <meshStandardMaterial />
+  //       </mesh>
+  //     </Canvas>
+  //   </>
+  // );
 }

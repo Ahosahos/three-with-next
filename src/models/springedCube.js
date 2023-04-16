344da9750a65c34da8a58c4173e1a9924c166620
sofiah <sofiahsabihah@webbytes.com.my>
2023-04-16 22:32:49 +0800

    hover with colors

--- src/models/springedCube.js
+++ src/models/springedCube.js
@@ -1,15 +1,24 @@
 import React, { useRef, useState } from "react";
 import { useSpring, animated, config } from "@react-spring/three";
 
+const setOfColors = ["green", "red", "hotpink", "purple", "blue"];
+const colorLength = setOfColors.length;
+
 export default function SpringedCube(props) {
   const [active, setActive] = useState(false);
-
+  const [hovered, setHover] = useState(false);
   const mesh = useRef();
+  const [curIdxColor, setCurIdxColor] = useState(0);
 
   const { scale } = useSpring({
     scale: active ? 1.5 : 1,
     config: config.wobbly,
   });
+
+  const onHover = () => {
+    setCurIdxColor(Math.floor(Math.random() * colorLength));
+    setHover(true);
+  };
 
   return (
     <animated.mesh
@@ -17,9 +26,13 @@ export default function SpringedCube(props) {
       ref={mesh}
       scale={scale}
       onClick={() => setActive(!active)}
+      onPointerOver={onHover}
+      onPointerOut={(e) => setHover(false)}
     >
       <boxGeometry args={[2, 2, 2]} />
-      <meshBasicMaterial color={"yellow"} />
+      <meshStandardMaterial
+        color={hovered ? setOfColors[curIdxColor] : "yellow"}
+      />
     </animated.mesh>
   );
 }
